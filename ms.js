6d0b8547d4315a1b6ec8f30d4f5e92f1a26b3e8d
ObedGS <jogs.12345@gmail.com>
2021-12-05 14:49:16 -0700

    Contactame funcional

---
+++ ms.js
@@ -0,0 +1,12 @@
+const $form = document.querySelector("#form");
+const $buttonMailto = document.querySelector('#correo')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+	event.preventDefault()
+	const form = new FormData(this)
+	$buttonMailto.setAttribute('href',`mailto:designer.obedgarfias@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('message')}`)
+	$buttonMailto.click();
+
+}
